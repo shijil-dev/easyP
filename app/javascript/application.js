dca308923d8dd2e944e5e80b2911177890b3c6ae
devan <devan@shjil.dev>
2024-03-06 19:23:05 +0530

    bootstrap-icons

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,4 +3,3 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "htmx.org"
 import * as bootstrap from "bootstrap"
-
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "htmx.org" # @1.9.10
 pin "bootstrap",to: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+
